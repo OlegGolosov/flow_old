b6992e812306383007dab965b19e497f6fe8efcb
OlegGolosov <ogolosov@lxbk0199.gsi.de>
2018-04-23 00:18:30 +0200

    loop through filelists

--- macro/batch/run.sh
+++ macro/batch/run.sh
@@ -8,33 +8,35 @@ MY_PATH=/lustre/nyx/cbm/users/ogolosov
 BATCH_DIR=$MY_PATH/flow/macro/batch
 LOG_DIR=$MY_PATH/NA49_flow/log
 EXE_DIR=$MY_PATH/flow/build
-#FILE_LIST=$MY_PATH/NA49_data/fileList.txt
-#FILE_LIST=/lustre/nyx/cbm/users/ogolosov/NA49_data/fileLists/40GeV-minbias.list
 CENTRALITY=$MY_PATH/tpc_centr.root
 FLOW_DIR=$MY_PATH/NA49_flow
+#FILE_LIST=$MY_PATH/NA49_data/fileList.txt
+FILE_LISTS=$MY_PATH/NA49_data/fileLists/data
 
-OUT_DIR=$FLOW_DIR/new/pimin
 
-#OUT_DIR=$OUT_DIR/01D
-#FILE_LIST=/lustre/nyx/cbm/users/ogolosov/NA49_data/fileLists/1_4std--40GeV-minbias-01D.list
-
-OUT_DIR=$OUT_DIR/02C
-FILE_LIST=/lustre/nyx/cbm/users/ogolosov/NA49_data/fileLists/1_4std+-40GeV-minbias-02C.list
-
+FLOW_DIR=$FLOW_DIR/default_old_y/pimin
 MIN_STEP=1
 MAX_STEP=2
 
-#split -n l/10 -d -a 3 filelist.txt filelist_
 
-#source $ROOT_PATH
+for FILE_LIST in $FILE_LISTS/*
+do
+  OUT_DIR=$FLOW_DIR/$(basename "$FILE_LIST")
+	echo $FILE_LIST
+	echo $OUT_DIR
+	
+  #split -n l/10 -d -a 3 filelist.txt filelist_
 
-mkdir -p $OUT_DIR
-mkdir -p $LOG_DIR
-mkdir -p $LOG_DIR/out
-mkdir -p $LOG_DIR/error
+  #source $ROOT_PATH
 
-sbatch --partition main -D $LOG_DIR --export=ALL $BATCH_DIR/run_kronos.sh $EXE_DIR $OUT_DIR $FILE_LIST $CENTRALITY $MIN_STEP $MAX_STEP $BATCH_DIR
+  mkdir -p $OUT_DIR
+  mkdir -p $LOG_DIR
+  mkdir -p $LOG_DIR/out
+  mkdir -p $LOG_DIR/error
 
-#. $BATCH_DIR/run_kronos.sh $EXE_DIR $OUT_DIR $FILE_LIST $CENTRALITY $MIN_STEP $MAX_STEP $BATCH_DIR
+  sbatch --partition main -D $LOG_DIR --export=ALL $BATCH_DIR/run_kronos.sh $EXE_DIR $OUT_DIR $FILE_LIST $CENTRALITY $MIN_STEP $MAX_STEP $BATCH_DIR
 
-# sbatch --partition main -D "/lustre/nyx/cbm/users/$USER/log" --export=ALL run_qa.sh na61
+#  . $BATCH_DIR/run_kronos.sh $EXE_DIR $OUT_DIR $FILE_LIST $CENTRALITY $MIN_STEP $MAX_STEP $BATCH_DIR
+
+#  sbatch --partition main -D "/lustre/nyx/cbm/users/$USER/log" --export=ALL run_qa.sh na61
+done
