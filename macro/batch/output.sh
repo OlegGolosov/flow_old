f6afe8f0c65286c1bee016f6961eb9cd7bce3174
Oleg <oleg.golosov@gmail.com>
2018-04-24 21:14:14 +0300

    f

--- macro/batch/output.sh
+++ macro/batch/output.sh
@@ -8,12 +8,12 @@ MERGE=$2
 for (( step=1; step<=2; step++ ))
 do  
 	if [ $MERGE != 0 ]; then
-		hadd -f $INDIR/corr_$step.root $INDIR/*/corr_$step.root
+		hadd -f $INDIR/corr_$step.root $INDIR/*/*/corr_$step.root
 	fi
 	root -q ../../rootlogon.C '../save_graphs.C ("'$INDIR/corr_$step.root'","'$INDIR/graph_$step.root'")'
 done
 
 if [ $MERGE != 0 ]; then
-	hadd -f $INDIR/qa.root $INDIR/*/qa.root
+	hadd -f $INDIR/qa.root $INDIR/*/*/qa.root
 fi
 root -q -b '../draw_graphs.C ("'$INDIR'")'
