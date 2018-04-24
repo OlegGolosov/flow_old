0117bed3e89ad62f8dce8b90f50b652d9b952922
Oleg <oleg.golosov@gmail.com>
2018-04-24 21:08:36 +0300

    sdfds

--- macro/batch/output.sh
+++ macro/batch/output.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 INDIR=$1
+MERGE=$2
 
 . set_path.sh
 
 for (( step=1; step<=2; step++ ))
 do  
-	hadd -f $INDIR/corr_$step.root $INDIR/*/corr_$step.root
+	if [ $MERGE != 0 ]; then
+		hadd -f $INDIR/corr_$step.root $INDIR/*/corr_$step.root
+	fi
 	root -q ../../rootlogon.C '../save_graphs.C ("'$INDIR/corr_$step.root'","'$INDIR/graph_$step.root'")'
 done
 
-hadd -f $INDIR/qa.root $INDIR/*/qa.root
+if [ $MERGE != 0 ]; then
+	hadd -f $INDIR/qa.root $INDIR/*/qa.root
+fi
 root -q -b '../draw_graphs.C ("'$INDIR'")'
